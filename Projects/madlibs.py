2c7bf5513105307d7de5857e1ca238faa167cae4
Anthony <anthonygacayan@yahoo.com>
2021-04-22 16:49:07 -0700

    madlib

---
+++ Projects/madlibs.py
@@ -0,0 +1,42 @@
+#! python3
+# madlibs.py - reads a text files and look for word ADJECTIVES,NOUN,ADVERB or VERB and prompts users to replace it
+
+import re
+
+#TODO: Read a text file.
+
+madlibsfile = open('madlibs.txt')
+content = madlibsfile.read()
+
+textcontent = re.split(r'\W+', content)
+#TODO: Look for word ADJECTIVES,NOUN,ADVERB or VERB. Prompt user to replace occurences.
+output = content
+
+for i in textcontent:
+    if i == 'ADJECTIVE':
+        textRegex = re.compile(r'(ADJECTIVE)')
+        print("Enter an ADJECTIVE:")
+        adj = input()
+        output = textRegex.sub(adj,output,1)
+    elif i == 'NOUN':
+        textRegex = re.compile(r'(NOUN)')
+        print("Enter a NOUN:")
+        noun = input()
+        output = textRegex.sub(noun,output,1)
+    elif i == 'ADVERB':
+        textRegex = re.compile(r'(ADVERB)')
+        print("Enter an ADVERB:")
+        adv = input()
+        output = textRegex.sub(adv,output,1)
+    elif i == 'VERB':
+        textRegex = re.compile(r'(VERB)')
+        print("Enter a VERB:")
+        verb = input()
+        output = textRegex.sub(verb,output,1)
+
+
+#TODO: print the result in the screen and save to new text file.
+newmadlibfile = open('newMadlib.txt', 'w')
+newmadlibfile.write(output)
+newmadlibfile.close
+print(output)
