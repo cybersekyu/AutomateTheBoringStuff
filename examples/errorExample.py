c9d534c52a653812a2edaa64674218075caa5db3
Anthony <anthonygacayan@yahoo.com>
2021-06-10 15:31:38 -0700

    chap10

---
+++ examples/errorExample.py
@@ -0,0 +1,18 @@
+'''def spam():
+    bacon()
+
+def bacon():
+    raise Exception('This is the error message')
+
+
+spam()'''
+
+import traceback
+
+try:
+    raise Exception('This is the error message')
+except:
+    errorFile = open('C:\\Users\\antho\\Documents\\TestingDir\\errorInfo.txt', 'w')
+    errorFile.write(traceback.format_exc())
+    errorFile.close()
+    print('The traceback info was written to errorInfo.txt')
